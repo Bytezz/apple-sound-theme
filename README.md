c7a3760fa0fcadd258ed7e7f43dc9f3d19103db2
Bytez <25850701+Bytezz@users.noreply.github.com>
2022-12-01 14:38:34 +0100

    Installation script and README section

--- README.md
+++ README.md
@@ -7,6 +7,28 @@ Apple sounds for all desktop environment xdg compliant.
 - More notification sounds
 - More ringtones
 
+## Install
+
+### Manual
+
+#### Terminal way
+
+`mkdir -p ~/.local/share/sounds/apple-sound-theme && cp -r * ~/.local/share/sounds/apple-sound-theme/`
+
+#### GUI way
+
+Copy apple-sound-theme folder into "~/.local/share/sounds/" (~ = /home/username)
+
+### Script
+
+#### Terminal way
+
+`./install.sh`
+
+#### GUI way
+
+Execute (usually double tap icon) install.sh.
+
 ## License
 
 [GPLv3](LICENSE)
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+window_title="Apple Sound Theme"
+ask_user_installation="Install Apple XDG Sound Theme for current user?"
+successfull_installation="Installation completed."
+failed_installation="Installation failed."
+
+useKdialog=false
+if [ "$XDG_CURRENT_DESKTOP" == "KDE" ]; then
+	useKdialog=true
+fi
+
+install_path="~/.local/share/sounds/apple-sound-theme"
+
+if $useKdialog ;then
+	kdialog --title "$window_title" --yesno "$ask_user_installation"
+	answer=$?
+else
+	zenity --question --title "$window_title" --text "$ask_user_installation"
+	answer=$?
+fi
+
+if [ $answer -eq 0 ]; then
+	cd "$(readlink -f "$(dirname "$0")")"
+
+	mkdir -p "$install_path" && cp -r * "$install_path"
+	answer=$?
+
+	if [ $answer -eq 0 ]; then
+		if $useKdialog ;then
+			kdialog --title "$window_title" --msgbox "$successfull_installation"
+		else
+			zenity --title "$window_title" --info --text "$successfull_installation" --icon-name=object-select-symbolic
+		fi
+	else
+		if $useKdialog ;then
+			kdialog --title "$window_title" --error "$failed_installation Error code $answer."
+		else
+			zenity --title "$window_title" --error --text "$failed_installation Error code $answer."
+		fi
+	fi
+fi
